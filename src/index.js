fcb22afb2de9d09af6aca6914098a545cfad5a4e
Yevhen <yevhenlebid51@gmail.com>
2021-02-17 16:01:51 +0200

    fixed tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    if (!array.length) return 0
+    return Math.min(...array)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  return !array.length ? 0 : Math.max(...array)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  return !array.length ? 0 : array.reduce((sum, el) => sum + el, 0)/array.length
 }
+
